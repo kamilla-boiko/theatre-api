1fd17e3639bd2f151ccf9fcaf74d427a155fae9a
kamilla-boiko <kamilla.boyko@gmail.com>
2023-07-20 12:23:51 +0300

    Set up local Postgres db

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -77,7 +77,13 @@ WSGI_APPLICATION = "theatre_service.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["DB_HOST"],
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+    }
 }
 
 # Password validation

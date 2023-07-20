c75d4084305d6df105e63502df8bc05de7c0e4f6
kamilla-boiko <kamilla.boyko@gmail.com>
2023-07-20 15:48:54 +0300

    THEATRE: VIEWS: Add basic viewsets for serializers

--- theatre/views.py
+++ theatre/views.py
@@ -1,9 +1,49 @@
 from rest_framework import viewsets
 
-from theatre.models import Play
-from theatre.serializers import PlaySerializer
+from theatre.models import (
+    Play,
+    Actor,
+    Genre,
+    TheatreHall,
+    Performance,
+    Reservation,
+)
+
+from theatre.serializers import (
+    PlaySerializer,
+    ActorSerializer,
+    GenreSerializer,
+    TheatreHallSerializer,
+    PerformanceSerializer,
+    ReservationSerializer,
+)
 
 
 class PlayViewSet(viewsets.ModelViewSet):
     queryset = Play.objects.all()
     serializer_class = PlaySerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TheatreHallViewSet(viewsets.ModelViewSet):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer

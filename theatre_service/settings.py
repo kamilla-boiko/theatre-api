08b08f4d95ba7292b8bbd9f12f82bc7ecbd6b29b
kamilla-boiko <kamilla.boyko@gmail.com>
2023-07-20 15:15:51 +0300

    Set up django debug toolbar

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "theatre",
     "user",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",

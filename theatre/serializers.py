644c23cf489ef767ed189521053e3721e6162637
kamilla-boiko <kamilla.boyko@gmail.com>
2023-07-20 15:47:27 +0300

    THEATRE: SERIALIZERS: Add basic serializers for models

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,9 +1,46 @@
 from rest_framework import serializers
 
-from theatre.models import Play
+from theatre.models import (
+    Play,
+    Actor,
+    Genre,
+    TheatreHall,
+    Performance,
+    Reservation,
+)
 
 
 class PlaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Play
         fields = ("id", "title", "description", "actors", "genres")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name")
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row")
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Performance
+        fields = ("id", "play", "theatre_hall", "show_time")
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ("id", "tickets", "user", "created_at")
